b0fdfd80f07d5f7aef86d3f0e9fcd40690924ce0
aleksanrpartnenko <aleksanrpartnenko@gmail.com>
2018-07-28 12:03:06 +0300

    day 2 crud without U

--- homework/homework/static/main.js
+++ homework/homework/static/main.js
@@ -1,8 +1,40 @@
-app = angular.module 'example.app.basic', []
+var app = angular.module('toDo', []);
+app.controller('toDoController', function($scope, $http) {
+    // $scope.todoList = [{todoText: 'Finish this app', done: false}];
+    $http.get('/plates').then(function(response) {
+        $scope.todoList = [];
+        for (var i = 0; i < response.data.length; i++) {
 
-app.controller 'AppController', ['$scope', '$http', ($scope, $http) ->
-    $scope.posts = []
-    $http.get('/plates').then (result) ->
-        angular.forEach result.data, (item) ->
-            $scope.posts.push item
-]
+            var todo = {};
+            todo.Name = response.data[i].NAME
+            todo.Plate = response.data[i].PLATE
+            todo.Remove = false
+            todo.id = response.data[i].id
+            $scope.todoList.push(todo);
+        }
+        console.log (response.data)
+        
+    });
+    $scope.saveData = function() {
+        var data = {NAME: $scope.todoInput_NAME, PLATE: $scope.todoInput_PLATE, }
+        $http.post('/plates', data)
+        console.log (data)
+    }
+    $scope.todoAdd = function() {
+        $scope.todoList.push({NAME: $scope.todoInput_NAME, PLATE: $scope.todoInput_PLATE, });
+        $scope.todoInput_NAME = '';
+        $scope.todoInput_PLATE = '';
+    };
+    $scope.remove = function() {
+        var oldList = $scope.todoList;
+        //console.log (todo)
+        $scope.todoList = [];
+        angular.forEach(oldList, function(todo) {
+            if (todo.Remove) {
+                $http.delete('/plates/' + todo.id + '/');
+            } else {
+                $scope.todoList.push(todo);
+            }
+        })
+    }
+})
